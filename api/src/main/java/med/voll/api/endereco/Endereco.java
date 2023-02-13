aa2514da70ca438da90a9714f9869aaf7da42149
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-02-13 10:31:27 -0300

    Create Endereco.java

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,12 @@
+package med.voll.api.endereco;
+
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String numero;
+    private String complemento;
+}
