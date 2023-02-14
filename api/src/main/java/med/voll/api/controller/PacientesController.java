a5b7095e55b02a597fb4e666e15efe8be5ae322f
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-02-14 09:50:46 -0300

    Update PacientesController.java

--- api/src/main/java/med/voll/api/controller/PacientesController.java
+++ api/src/main/java/med/voll/api/controller/PacientesController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pacientes")
+@RequestMapping("pacientes")
 public class PacientesController {
 
     private PacienteRepository repository;
