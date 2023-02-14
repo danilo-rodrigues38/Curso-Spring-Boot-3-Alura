bc5db895c1c40cf6ad9b4a84a3796cc5dff42fcf
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-02-14 15:56:16 -0300

    Update PacientesController.java

--- api/src/main/java/med/voll/api/controller/PacientesController.java
+++ api/src/main/java/med/voll/api/controller/PacientesController.java
@@ -1,15 +1,17 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Medico;
 import med.voll.api.paciente.DadosCadastroPacientes;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("pacientes")
@@ -22,5 +24,10 @@ public class PacientesController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPacientes dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemPaciente> listar() {
+        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
     }
 }
