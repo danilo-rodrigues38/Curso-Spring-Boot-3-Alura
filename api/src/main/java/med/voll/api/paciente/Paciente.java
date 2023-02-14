382b7aa578fd16cddd339a57a6c67c8e13efae50
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-02-14 09:41:31 -0300

    Create Paciente

---
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,34 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPacientes dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
