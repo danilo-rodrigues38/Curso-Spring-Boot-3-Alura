ea44356afb820bd73cead9283f7b2d597259e003
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-02-14 09:44:43 -0300

    Update PacientesController.java

--- api/src/main/java/med/voll/api/controller/PacientesController.java
+++ api/src/main/java/med/voll/api/controller/PacientesController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import med.voll.api.paciente.DadosCadastroPacientes;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class PacientesController {
     private PacienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroPacientes dados) {
         repository.save(new Paciente(dados));
     }
