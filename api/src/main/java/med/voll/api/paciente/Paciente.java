adea7acb30abd90eb7bd7da4e4344eb6bbdf67a8
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-02-15 09:23:34 -0300

    Update Paciente.java

--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -35,14 +35,18 @@ public class Paciente {
     }
 
     public void atualizarInformacoes(DadosAtualizacaoPacientes dados) {
-        if (dados.nome() != null)  {
+        if (dados.nome() != null) {
             this.nome = dados.nome();
         }
-        if (dados.telefone() != null)  {
+        if (dados.telefone() != null) {
             this.telefone = dados.telefone();
         }
-        if (dados.endereco() != null)  {
+        if (dados.endereco() != null) {
             this.endereco.atualizarInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
