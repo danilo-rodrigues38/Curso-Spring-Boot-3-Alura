6be9d950456927b1aaaaf60ed627f8b88ded3a52
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-02-16 12:32:01 -0300

    Update MedicosController.java

--- api/src/main/java/med/voll/api/controller/MedicosController.java
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -71,4 +71,10 @@ public class MedicosController {
         medico.ativar();
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
 }
